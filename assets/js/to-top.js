9d8c826bd672bf905bc2001813d7ec7462af0d21
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-01-08 14:27:33 +0900

    Compile

--- assets/js/to-top.js
+++ assets/js/to-top.js
@@ -1,13 +1,2 @@
-(function() {
-  $(function() {
-    return $(".to-top").click(function() {
-      $("body, html").animate({
-        scrollTop: 0
-      }, 400);
-      return false;
-    });
-  });
-
-}).call(this);
-
+(function(){$(function(){return $(".to-top").click(function(){return $("body, html").animate({scrollTop:0},400),!1})})}).call(this);
 //# sourceMappingURL=.map/to-top.js.map
