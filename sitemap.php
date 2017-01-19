6c7cde037622ef12436de6e8bd49024b6a72d680
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-01-19 19:42:27 +0900

    Rewrite sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -5,6 +5,153 @@ Use:php sitemap-github.php > sitemap-github.xml
 */
 
 /**
+ * Class SiteMap
+ */
+class SiteMap
+{
+    /**
+     * DOMDocument instance.
+     *
+     * @var DOMDocument
+     */
+    protected $domDocument;
+
+    /**
+     * \DOMElement instance.
+     *
+     * @var \DOMElement
+     */
+    protected $domElement;
+
+    /**
+     * Lastmod date format.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d';
+
+    /**
+     * SiteMap constructor.
+     */
+    public function __construct()
+    {
+        $this->domDocument                     = new DOMDocument('1.0', 'UTF-8');
+        $this->domDocument->preserveWhiteSpace = false;
+        $this->domDocument->formatOutput       = true;
+        $this->domElement                      = $this->domDocument->appendChild($this->domDocument->createElement('urlset'));
+        $this->domElement->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+    }
+
+    /**
+     * Get date format.
+     *
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * Add url.
+     *
+     * @param array $info
+     *
+     * @return $this
+     */
+    public function addUrl(array $info)
+    {
+        $url = $this->domElement->appendChild($this->domDocument->createElement('url'));
+        foreach ($info as $key => $value) {
+            if (is_string($key) && is_string($value)) {
+                $url->appendChild($this->domDocument->createElement($key, $value));
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add urls.
+     *
+     * @param array $urls
+     *
+     * @return $this
+     */
+    public function addUrls(array $urls)
+    {
+        foreach ($urls as $info) {
+            $this->addUrl($info);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return XML string.
+     *
+     * @return string
+     */
+    public function saveXML()
+    {
+        return $this->domDocument->saveXML();
+    }
+
+    /**
+     * Return GitHub Pages url.
+     *
+     * @param string $path
+     * @return string
+     */
+    public function getUrl($path)
+    {
+        return "https://hiroto-k.github.io{$path}";
+    }
+
+    /**
+     * Get last modified
+     * @param string $path
+     * @return string
+     */
+    public function getLastModified($path)
+    {
+        $headers = get_headers($this->getUrl($path));
+        $header = array_values(array_filter($headers, function ($s) {
+            return preg_match("/Last-Modified:\s+(.+)/i", $s) === 0 ? false : true;
+        }));
+        preg_match("/Last-Modified:\s+(.+)/i", $header[0], $matches);
+        return date($this->getDateFormat(), strtotime($matches[1]));
+    }
+
+    /**
+     * Get api gen document pages
+     * @param string $repoName
+     * @return array
+     */
+    public function getApiGenPages($repoName) {
+        $url = $this->getUrl($repoName);
+        $content = file_get_contents($url);
+        preg_match_all('/<a href="(.+)">/', $content, $m);
+        $m = $m[1];
+        array_pop($m);
+        $urls = array_map(function ($u) use ($repoName) {
+            return $repoName.$u;
+        }, $m);
+        return $urls;
+    }
+
+    /**
+     * Cast to string.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->saveXML();
+    }
+}
+
+/**
  * Class SiteMapGenerator
  */
 class SiteMapGenerator {
@@ -127,7 +274,7 @@ class SiteMapGenerator {
   }
 }
 
-$siteMap = new SiteMapGenerator();
+$siteMap = new SiteMap();
 
 $sites = [
   [
