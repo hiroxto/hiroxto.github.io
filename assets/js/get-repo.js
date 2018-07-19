ddcefe56d033af0fb798c6c7e9f8a257134cd6bb
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-07-19 22:46:49 +0900

    Remove get-repo.js

--- assets/js/get-repo.js
+++
@@ -1,2 +0,0 @@
-"use strict";$(function(){$.getJSON("https://api.github.com/users/Hiroto-K/repos",{per_page:"100"},function(t,e,s){"success"===e?($(".repo-count").html(t.length),t.forEach(function(t){$(".repo-list").append('<a href="'+t.html_url+'" class="list-group-item">'+t.full_name+"</a>"),t.has_pages&&$(".pages-list").append('<a href="https://hiroto-k.github.io/'+t.name+'" class="list-group-item">'+t.full_name+"</a>")})):(console.log(e),$(".github-main").hide(),$(".github-error").show())})});
-//# sourceMappingURL=.map/get-repo.js.map
