b6953a93376ffc1f7d5105af0bc2bab968ba3a2c
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-07-23 22:31:17 +0900

    Rewrite lint tasks

---
+++ gulp/tasks/lint.js
@@ -0,0 +1,13 @@
+const gulp = require("gulp");
+const config = require("../config");
+const $ = config.plugins;
+
+gulp.task("lint", ["lint:es"]);
+
+gulp.task("lint:es", () => {
+    gulp
+        .src(config.src.babel)
+        .pipe($.plumber())
+        .pipe($.eslint(config.options.eslint))
+        .pipe($.eslint.formatEach("compact", process.stderr));
+});
