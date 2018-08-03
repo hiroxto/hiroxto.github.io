b12250c7eab58dc4b3db93b6eaa28d9b69291206
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-03 23:34:49 +0900

    Fix the coding styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,24 +1,24 @@
-const gulp = require("gulp");
-const config = require("../config");
+const gulp = require('gulp');
+const config = require('../config');
 const $ = config.plugins;
 
-gulp.task("watch", () => {
-    [
-        "vue",
-        "scss",
-    ].forEach((name) => {
-        gulp.start(`watch:${name}`)
-    });
+gulp.task('watch', () => {
+  [
+    'vue',
+    'scss'
+  ].forEach((name) => {
+    gulp.start(`watch:${name}`);
+  });
 });
 
-gulp.task("watch:vue", () => {
-    $.watch(config.src.vue_watch, () => {
-        gulp.start("compile:vue")
-    });
+gulp.task('watch:vue', () => {
+  $.watch(config.src.vue_watch, () => {
+    gulp.start('compile:vue');
+  });
 });
 
-gulp.task("watch:scss", () => {
-    $.watch(config.src.scss, () => {
-        gulp.start("compile:scss")
-    });
+gulp.task('watch:scss', () => {
+  $.watch(config.src.scss, () => {
+    gulp.start('compile:scss');
+  });
 });
