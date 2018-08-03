e0bb7c1ed1c689cc5854fe2470b32338a91f1c0e
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-04 01:39:07 +0900

    Update eslint configures

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,16 @@ module.exports = {
         'semi': ['error', 'always'],
         'no-console': 'off',
         'no-new': 'off',
+        'indent': ['error', 2],
         "vue/valid-v-for": "off",
-        "vue/script-indent": ["error", 2, {"baseIndent": 1}]
-    }
+        "vue/script-indent": ["error", 2, {baseIndent: 1, switchCase: 1}]
+    },
+    'overrides': [
+        {
+            'files': ['*.vue'],
+            'rules': {
+                'indent': 'off'
+            }
+        }
+    ],
 };
