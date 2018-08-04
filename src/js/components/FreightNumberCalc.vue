d0af67c791aa3e11b51b725947d1c85c99ad0aa7
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-04 14:08:41 +0900

    Create the templates

--- src/js/components/FreightNumberCalc.vue
+++ src/js/components/FreightNumberCalc.vue
@@ -1,5 +1,10 @@
 <template>
-
+  <div class="jumbotron">
+    <div class="container">
+      <h1>列番計算</h1>
+      <p class="lead">任意の2〜4桁の数字の列番から列車種別を計算。</p>
+    </div>
+  </div>
 </template>
 
 <script>
