048ff4400092b3967f4f46ba373c7e21b706ffa8
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-04 12:55:51 +0900

    Fix indents

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
-    root: true,
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: [
-        'plugin:vue/essential',
-        'standard'
-    ],
-    plugins: [
-      'vue'
-    ],
-    rules: {
-        'semi': ['error', 'always'],
-        'no-console': 'off',
-        'no-new': 'off',
-        'indent': ['error', 2],
-        "vue/valid-v-for": "off",
-        "vue/script-indent": ["error", 2, {baseIndent: 1, switchCase: 1}]
-    },
-    'overrides': [
-        {
-            'files': ['*.vue'],
-            'rules': {
-                'indent': 'off'
-            }
-        }
-    ],
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+    'no-console': 'off',
+    'no-new': 'off',
+    'indent': ['error', 2],
+    "vue/valid-v-for": "off",
+    "vue/script-indent": ["error", 2, {baseIndent: 1, switchCase: 1}]
+  },
+  'overrides': [
+    {
+      'files': ['*.vue'],
+      'rules': {
+        'indent': 'off'
+      }
+    }
+  ],
 };
