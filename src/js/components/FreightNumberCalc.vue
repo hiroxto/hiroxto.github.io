5cb3ac617f358b9a7b2941e6dde08c666326c8eb
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-04 14:46:40 +0900

    Create templates and methods

--- src/js/components/FreightNumberCalc.vue
+++ src/js/components/FreightNumberCalc.vue
@@ -1,8 +1,38 @@
 <template>
   <div class="jumbotron">
     <div class="container">
+
       <h1>列番計算</h1>
       <p class="lead">任意の2〜4桁の数字の列番から列車種別を計算。</p>
+
+      <div v-if="errors.length">
+        <div class="alert alert-warning" role="alert">
+          <ul>
+            <li v-for="error in errors">
+              {{ error }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div v-else-if="freightType !== null">
+        <div class="alert alert-info" role="alert">
+          {{ freightType }}
+        </div>
+      </div>
+
+      <form>
+        <div class="form-group">
+          <label for="freightNumber">列車番号 (2〜4ケタの数字)</label>
+          <input
+            v-model.number="freightNumber"
+            type="number"
+            class="form-control"
+            id="freightNumber"
+            placeholder="列車番号"
+          >
+        </div>
+      </form>
+
     </div>
   </div>
 </template>
@@ -10,6 +40,44 @@
 <script>
   export default {
     name: 'FreightNumberCalc',
+    data() {
+      return {
+        errors: [],
+        freightNumber: null,
+      }
+    },
+    computed: {
+      freightType: function () {
+        return this.calcNumber(this.freightNumber);
+      },
+    },
+    methods: {
+      calcNumber: function (number) {
+        if (number === null || number === '') {
+          return null;
+        }
+
+        const valid = this.validateNumber(number);
+
+        if (valid === false){
+          return;
+        }
+
+        return '種別';
+      },
+      validateNumber: function (number) {
+        this.errors = [];
+
+        if (!Number.isInteger(number)) {
+          this.errors.push('入力された数が整数ではありません。')
+        }
+        else if (number < 50 || number > 9999) {
+          this.errors.push('入力された数の範囲が貨物列車の列車番号ではありません。')
+        }
+
+        return this.errors.length === 0;
+      },
+    }
   };
 </script>
 
