d9d260543cff01325ec2f6684d824eaf9ea5a94c
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-12 19:43:34 +0900

    Use the bundle bootstrap file

--- gulp/config.js
+++ gulp/config.js
@@ -19,7 +19,7 @@ let config = {
     lib: {
       js: [
         './node_modules/jquery/dist/jquery.slim.min.js',
-        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
       ],
     },
   },
