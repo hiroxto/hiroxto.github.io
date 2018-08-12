4d4ac9c279e782fe59312324df651138aadd902c
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-12 19:49:08 +0900

    Don't generate source maps in library compile

--- gulp/tasks/lib.js
+++ gulp/tasks/lib.js
@@ -8,9 +8,7 @@ gulp.task('lib', ['lib:js']);
 gulp.task('lib:js', () => {
   gulp
     .src(config.src.lib.js)
-    .pipe($.if(!isProduction, $.sourcemaps.init()))
     .pipe($.if(isProduction, $.uglify()))
     .pipe($.concat(config.lib.js))
-    .pipe($.if(!isProduction, $.sourcemaps.write(config.map)))
     .pipe(gulp.dest(config.dist.js));
 });
