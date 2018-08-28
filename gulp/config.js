059ea63abdcfc15e30eea87d9c729c8ca27f3350
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-08-28 18:27:41 +0900

    Fix lib js files

--- gulp/config.js
+++ gulp/config.js
@@ -27,7 +27,8 @@ let config = {
     lib: {
       js: [
         './node_modules/jquery/dist/jquery.slim.min.js',
-        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        './node_modules/popper.js/dist/popper.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
       ],
     },
   },
