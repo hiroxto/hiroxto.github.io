02fbd7456c76513573171669dce77d2119976d50
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-09-02 16:51:42 +0900

    Fix the datetime format

--- update-sitemap.sh
+++ update-sitemap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use "bash update-sitemap.sh"
 php sitemap.php > sitemap.xml
-d=`date +"%Y/%m/%d %k:%M:%S"`
+d=`date +"%Y/%m/%d %k:%M:%S %z"`
 git add sitemap.xml
 git diff --cached --exit-code --quiet || git commit -m "Sitemap update at $d"
