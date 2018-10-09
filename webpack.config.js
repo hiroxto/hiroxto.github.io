942376b89850e07a47ff47c69d9722ea88888ae1
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-10-09 23:01:16 +0900

    Add base configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,37 @@
+const path = require('path');
+
 module.exports = {
+  entry: {
+    'app': './src/js/app.js',
+    'train-number-calc': './src/js/train-number-calc.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './'),
+  },
 };
