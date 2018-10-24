92076647c889342c33c2b9657cc1a0eb30a0ad7b
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-10-24 19:32:33 +0900

    Import and use vee-validate

--- src/js/train-number-calc.js
+++ src/js/train-number-calc.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import VeeValidate from 'vee-validate';
 import TrainNumberCalc from './components/TrainNumberCalc';
 
 import './bootstrap';
 import './google-analytics';
+
+Vue.use(VeeValidate);
 
 new Vue({
   el: '#app',
