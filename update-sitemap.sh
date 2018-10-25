6d6e00d65b127d67e0ea2b53548d9f04ed3f4692
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-10-26 04:15:40 +0900

    Use the "sitemap.js"

--- update-sitemap.sh
+++ update-sitemap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use "bash update-sitemap.sh"
-php sitemap.php > sitemap.xml
+node sitemap.js > sitemap.xml
 d=`date +"%Y/%m/%d %k:%M:%S %z"`
 git add sitemap.xml
 git diff --cached --exit-code --quiet || git commit -m "Sitemap update at $d"
