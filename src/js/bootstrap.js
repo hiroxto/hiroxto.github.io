f1491d8e9f2eec1aca1a95baf872190fd8ba4685
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-10-30 17:14:18 +0900

    Use the bootstrap-vue in bootstrap.js

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import Footer from './components/Footer';
 
-import 'bootstrap/dist/js/bootstrap.bundle';
+// import 'bootstrap/dist/js/bootstrap.bundle';
+
+Vue.use(BootstrapVue);
 
 Vue.component('page-footer', Footer);
