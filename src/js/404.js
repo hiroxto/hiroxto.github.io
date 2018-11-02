24fdb1e259c3e9d95e001cdb01d396e865c8f7a3
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-11-03 02:26:33 +0900

    Move the templates to "404.js" from html

--- src/js/404.js
+++ src/js/404.js
@@ -4,4 +4,16 @@ import './bootstrap';
 
 new Vue({
   el: '#app',
+  template: `
+    <div>
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">404 Not Found</h1>
+          <hr>
+          <p class="lead">Page not found. Back to <a href="/">Top Page</a>.</p>
+        </div>
+      </div>
+      <page-footer></page-footer>
+    </div>
+  `,
 });
