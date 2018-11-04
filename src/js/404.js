df6fa47c77541df718c36ee1c0a6ad12bf6b6f20
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-11-04 21:57:18 +0900

    Use the bootstrap-vue in 404 page

--- src/js/404.js
+++ src/js/404.js
@@ -6,13 +6,11 @@ new Vue({
   el: '#app',
   template: `
     <div>
-      <div class="jumbotron">
-        <div class="container">
-          <h1 class="display-3">404 Not Found</h1>
+      <b-jumbotron>
+          <h1>404 Not Found</h1>
           <hr>
-          <p class="lead">Page not found. Back to <a href="/">Top Page</a>.</p>
-        </div>
-      </div>
+          <p class="lead">Page not found. Back to <a href="/">Top Page</a>.</p>      
+      </b-jumbotron>
       <page-footer></page-footer>
     </div>
   `,
