8bc74399d2c8df7a70a77bab2a7853ee99df8b49
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-11-10 17:15:44 +0900

    Remove empty line from train-number-calc.js

--- src/js/train-number-calc.js
+++ src/js/train-number-calc.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import VeeValidate, { Validator } from 'vee-validate';
 import TrainNumberCalc from './components/TrainNumberCalc';
 import ja from 'vee-validate/dist/locale/ja';
-
 import './bootstrap';
 import './google-analytics';
 
