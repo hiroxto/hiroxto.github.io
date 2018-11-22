76bba17952bf3652cd8b7803e22290d920a26394
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-11-22 20:02:13 +0900

    Order the imports

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import Footer from './components/Footer';
 import VeeValidate, { Validator } from 'vee-validate';
 import ja from 'vee-validate/dist/locale/ja';
 import BootstrapVue from 'bootstrap-vue';
+import Footer from './components/Footer';
 
 Vue.use(VeeValidate);
 Validator.localize('ja', ja);
