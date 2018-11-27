d53580fa6c44850fcc4eee3d8684c954f1edffff
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-11-27 22:39:47 +0900

    Create value form in component

--- src/js/components/QrCode.vue
+++ src/js/components/QrCode.vue
@@ -4,6 +4,22 @@
       <h1>QR Code</h1>
       <p class="lead">QR Code generator.</p>
 
+      <b-form @submit.prevent="false">
+        <b-form-group
+          id="qrCodeValueInputGroup"
+          label="Value :"
+          label-for="qrCodeValueInput"
+        >
+          <b-form-input
+            id="qrCodeValueInput"
+            type="text"
+            v-model="value"
+            placeholder="Value"
+          >
+          </b-form-input>
+        </b-form-group>
+      </b-form>
+
       <qrcode-vue :value="value" size="200" level="H"></qrcode-vue>
     </b-jumbotron>
 
