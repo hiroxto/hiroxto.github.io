bb07088fd6f00edb57e11ebe9f594b4076084fbd
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-12-04 22:13:47 +0900

    Add vue/html-closing-bracket-newline rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'vue/valid-v-for': 'off',
     'vue/script-indent': ['error', 2, { baseIndent: 1, switchCase: 1 }],
     'vue/html-indent': ['error', 2, { attribute: 1, closeBracket: 0, baseIndent: 1 }],
+    'vue/html-closing-bracket-newline': ['error', { 'singleline': 'never', multiline: 'always' }],
   },
   overrides: [
     {
