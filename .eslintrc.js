316bb26b3a4b6a64142a875e0af6b65645e97de9
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-12-04 22:17:48 +0900

    Add vue/v-bind-style rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'vue/html-closing-bracket-newline': ['error', { 'singleline': 'never', multiline: 'always' }],
     'vue/html-quotes': ['error', 'double'],
     'vue/no-multi-spaces': ['error', { ignoreProperties: true }],
+    'vue/v-bind-style': ['error', 'shorthand'],
   },
   overrides: [
     {
