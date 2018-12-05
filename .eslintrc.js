41d5e68803eea43ffed90ccca9c0da2f47fc467c
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-12-06 00:58:47 +0900

    Add vue/html-self-closing rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'vue/no-multi-spaces': ['error', { ignoreProperties: true }],
     'vue/v-bind-style': ['error', 'shorthand'],
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/html-self-closing': ['error', { html: { 'void': 'never', 'normal': 'never', 'component': 'never' } }],
   },
   overrides: [
     {
