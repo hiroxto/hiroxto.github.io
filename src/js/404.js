60e76098611a544a76b0fccfdefd358d131f7302
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-12-15 22:42:30 +0900

    Remove hr from 404 page

--- src/js/404.js
+++ src/js/404.js
@@ -7,7 +7,6 @@ new Vue({
     <b-container>
       <b-jumbotron>
         <h1>404 Not Found</h1>
-        <hr>
         <p class="lead">Page not found. Back to <a href="/">Top Page</a>.</p>      
       </b-jumbotron>
       <page-footer></page-footer>
