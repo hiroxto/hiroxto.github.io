2e7630bcb5e1a9d8deff6d42dc60e6136278b4a6
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-01-16 17:14:07 +0900

    Update texts

--- src/js/components/TrainNumberCalc/HowToCalc.vue
+++ src/js/components/TrainNumberCalc/HowToCalc.vue
@@ -42,33 +42,33 @@
     computed: {
       passengerListHtmls: function () {
         return [
-          '桁数が1,2,4桁で、4桁のときの3桁目が<code>0</code>の場合、<strong>特急客</strong>。',
-          '桁数が3,4桁かつ、3桁目が<code>0</code>以外で下2桁が<code>00〜19</code>の場合、<strong>急客</strong>',
-          '桁数が3,4桁かつ、3桁目が<code>0</code>以外で下2桁が<code>20〜49</code>の場合、<strong>客</strong>',
-          '4桁目が<code>6</code>以上の場合は種別の頭に<code>臨</code>が付く。(e.g. <strong>臨特急</strong>, <strong>臨急客</strong>, <strong>臨客</strong>)',
+          '桁数が1,2,4桁で、4桁のときの百位が<code>0</code>の場合、<strong>特急客</strong>。',
+          '桁数が3,4桁かつ、百位が<code>0</code>以外で下2桁が<code>00〜19</code>の場合、<strong>急客</strong>',
+          '桁数が3,4桁かつ、百位が<code>0</code>以外で下2桁が<code>20〜49</code>の場合、<strong>客</strong>',
+          '千位が<code>6</code>以上の場合は種別の頭に<code>臨</code>が付く。(e.g. <strong>臨特急</strong>, <strong>臨急客</strong>, <strong>臨客</strong>)',
         ];
       },
       freightListHtmls: function () {
         return [
           {
-            html: '4桁目が<code>0～5</code>、3桁目が<code>0</code>、<strong>高速貨</strong>。下2桁で<strong>高速貨A</strong>と<strong>高速貨B</strong>に分かれる。',
+            html: '千位が<code>0～5</code>、百位が<code>0</code>の場合、<strong>高速貨</strong>。下2桁で<strong>高速貨A</strong>と<strong>高速貨B</strong>に分かれる。',
             list: [
               '下2桁が<code>50〜69</code>の場合、<strong>高速貨A</strong>。',
               '下2桁が<code>70〜99</code>の場合、<strong>高速貨B</strong>。',
             ],
           },
           {
-            html: '4桁目が<code>0,1</code>、3桁目が<code>1〜9</code>、下2桁が<code>50〜59</code>の場合、<strong>高速貨C</strong>。',
+            html: '千位が<code>0,1</code>、百位が<code>1〜9</code>、下2桁が<code>50〜59</code>の場合、<strong>高速貨C</strong>。',
           },
           {
-            html: '4桁目が<code>1,3,4,5</code>、3桁目が<code>1～9</code>の場合、<strong>専貨</strong>。下2桁で<strong>専貨A</strong>と<strong>専貨B</strong>に分かれる。',
+            html: '千位が<code>1,3,4,5</code>、百位が<code>1～9</code>の場合、<strong>専貨</strong>。下2桁で<strong>専貨A</strong>と<strong>専貨B</strong>に分かれる。',
             list: [
               '下2桁が<code>60～89</code>の場合、<strong>専貨A</strong>。',
               '下2桁が<code>90～99</code>の場合、<strong>専貨B</strong>。',
             ],
           },
           {
-            html: '4桁目が<code>8</code>以上の場合は種別の頭に<code>臨</code>が付く。(e.g. <strong>臨高速貨A</strong>, <strong>臨専貨A</strong>)',
+            html: '千位が<code>8</code>以上の場合は種別の頭に<code>臨</code>が付く。(e.g. <strong>臨高速貨A</strong>, <strong>臨専貨A</strong>)',
           },
         ];
       },
