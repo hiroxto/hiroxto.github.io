4d3a34e51111032593b8b6f6922f34788fba28a8
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-02-08 21:53:06 +0900

    Use the fontawesome-vue in bootstrap.js

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -2,12 +2,24 @@ import Vue from 'vue';
 import VeeValidate, { Validator } from 'vee-validate';
 import ja from 'vee-validate/dist/locale/ja';
 import BootstrapVue from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome, faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Footer from './components/Footer';
-import '@fortawesome/fontawesome-free/js/all';
 
 Vue.use(VeeValidate);
 Validator.localize('ja', ja);
 
 Vue.use(BootstrapVue);
+
+library.add(
+  faHome,
+  faUserAlt,
+  faTwitter,
+  faGithub,
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.component('page-footer', Footer);
