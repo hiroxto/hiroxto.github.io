347761380e874ad788e2e43e681b8fee46f5396b
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-10-09 23:24:47 +0900

    Remove the vee-validate codes

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
-import VeeValidate, { Validator } from 'vee-validate';
-import ja from 'vee-validate/dist/locale/ja';
 import BootstrapVue from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHome, faUserAlt } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Footer from './components/Footer';
-
-Vue.use(VeeValidate);
-Validator.localize('ja', ja);
 
 Vue.use(BootstrapVue);
 
