93dccea09b1376003706bbadcb56a602f1678100
hiroxto <hiroxto@gmail.com>
2021-10-09 19:46:33 +0900

    ESLintの設定をShareable Configに変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,4 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    'plugin:promise/recommended',
-    'standard',
-  ],
-  plugins: [
-    'promise',
-  ],
-  rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    semi: ['error', 'always'],
-    'no-console': 'off',
-    'no-new': 'off',
-    indent: ['error', 2],
-    'promise/catch-or-return': ['error', { allowThen: true, terminationMethod: ['catch', 'asCallback', 'finally'] }],
-  },
+  extends: ['@hiroxto'],
 };
